efc1982adc48c2b02fc0fc32d2b591ac3cb459b4
prateek.pathak@fissionlabs.com <prateek.pathak@fissionlabs.com>
2021-11-16 23:39:30 +0530

    updated

--- scheduler.js
+++ scheduler.js
@@ -251,6 +251,7 @@ module.exports = function (RED) {
                     } else {
                         node.send({ _msgid: msgid, req: req, res: createResponseWrapper(node, res) });
                     }
+                    done()
                 };
 
                 var httpMiddleware = function (req, res, next) { next(); }
